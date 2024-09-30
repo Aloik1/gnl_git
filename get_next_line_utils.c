b0126c31ee5ebc5d3faf01734e18b5a35999de09
Aloik1 <ilykond@gmail.com>
2024-09-30 18:45:07 +0200

    Works UwU

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,28 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikondrat <ikondrat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/24 15:44:49 by ikondrat          #+#    #+#             */
-/*   Updated: 2024/09/25 19:26:52 by ikondrat         ###   ########.fr       */
+/*   Created: 2024/09/16 18:29:59 by ikondrat          #+#    #+#             */
+/*   Updated: 2024/09/30 18:28:50 by ikondrat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_putchar_fd(char c, int fd)
+size_t	ft_strlen(const char *s)
 {
-	write(fd, &c, 1);
-}
-void	ft_putstr_fd(char *s, int fd)
-{
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
-	{
-		ft_putchar_fd(s[i], fd);
 		i++;
-	}
+	return (i);
 }
+
 char	*ft_substr(const char *str, unsigned int start, size_t len)
 {
 	char	*substr;
@@ -55,6 +50,7 @@ char	*ft_substr(const char *str, unsigned int start, size_t len)
 	substr[i] = '\0';
 	return (substr);
 }
+
 char	*ft_strdup(const char *s)
 {
 	size_t	i;
@@ -72,106 +68,7 @@ char	*ft_strdup(const char *s)
 	dup[i] = s[i];
 	return (dup);
 }
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-static char	*ft_strndup(const char *s, size_t n)
-{
-	size_t	i;
-	char	*dup;
-
-	i = 0;
-	dup = (char *)malloc(n * sizeof(char) + 1);
-	if (!dup)
-		return (NULL);
-	while (s[i] && i < n)
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
-}
-
-static size_t	count_words(const char *s, char c)
-{
-	size_t	i;
-	size_t	words;
-	size_t	inside;
-
-	i = 0;
-	words = 0;
-	inside = 0;
-	while (s[i])
-	{
-		if (s[i] != c && inside == 0)
-		{
-			words++;
-			inside = 1;
-		}
-		else if (s[i] == c)
-			inside = 0;
-		i++;
-	}
-	return (words);
-}
-
-static char	**ft_free(char **matrix, size_t j)
-{
-	while (j > 0)
-	{
-		free(matrix[j - 1]);
-		j--;
-	}
-	free(matrix);
-	return (NULL);
-}
-
-static char	**split_process(char **out, const char *s, char c)
-{
-	size_t	i;
-	size_t	j;
-	size_t	start;
-
-	i = 0;
-	j = 0;
-	while (j < count_words(s, c))
-	{
-		while (s[i] && s[i] == c)
-			i++;
-		start = i;
-		while (s[i] && s[i] != c)
-			i++;
-		if (i > start)
-		{
-			out[j] = ft_strndup(s + start, i - start);
-			if (!out[j])
-				return (ft_free(out, j));
-			j++;
-		}
-	}
-	out[j] = NULL;
-	return (out);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**out;
-
-	if (!s)
-		return (NULL);
-	out = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
-	if (!out)
-		return (NULL);
-	return (split_process(out, s, c));
-}
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*dest;
@@ -195,4 +92,22 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	}
 	dest[i + j] = '\0';
 	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*out;
+
+	i = 0;
+	out = (char *)s;
+	while (out[i])
+	{
+		if (out[i] == (char)c)
+			return (out + i);
+		i++;
+	}
+	if (out[i] == (char)c && out[i] == '\0')
+		return (out + i);
+	return (NULL);
 }
