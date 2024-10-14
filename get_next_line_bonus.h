f9e8de46727870770e3b1a21687c3524b9484051
Aloik1 <ilykond@gmail.com>
2024-10-14 13:22:03 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikondrat <ikondrat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 14:05:29 by ikondrat          #+#    #+#             */
+/*   Updated: 2024/09/30 18:34:15 by ikondrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	*ft_substr(const char *str, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+char	**ft_split(const char *s, char c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t n, size_t size);
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikondrat <ikondrat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 18:29:59 by ikondrat          #+#    #+#             */
+/*   Updated: 2024/09/30 18:28:50 by ikondrat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(const char *str, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	if (start >= ft_strlen(str))
+		return (ft_strdup(""));
+	if (len > ft_strlen(str) - start)
+		len = ft_strlen(str) - start;
+	substr = (char *)malloc(len * sizeof(char) + 1);
+	if (!substr)
+		return (NULL);
+	while (i < len)
+	{
+		substr[i] = str[start + i];
+		i++;
+	}
+	while (str[start + i] && i < len && start < ft_strlen(str))
+	{
+		substr[i] = str[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*dup;
+
+	i = 0;
+	dup = (char *)malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = s[i];
+	return (dup);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	dest = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char) + 1);
+	if (!dest)
+		return (NULL);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	*out;
+
+	i = 0;
+	out = (char *)s;
+	while (out[i])
+	{
+		if (out[i] == (char)c)
+			return (out + i);
+		i++;
+	}
+	if (out[i] == (char)c && out[i] == '\0')
+		return (out + i);
+	return (NULL);
+}
